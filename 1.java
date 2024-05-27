866c6fd26761f6ed5f45672095f7c07edce04483
shashank chaudhary <97469450+Shashankch14@users.noreply.github.com>
2024-05-28 00:05:44 +0530

    Create 1.java
    /*1.create  the given pattern 
    000000
    000000
    000000
    000000
    */

---
+++ 1.java
@@ -0,0 +1,18 @@
+
+/*1.create  the given pattern 
+000000
+000000
+000000
+000000
+*/
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        for(int i =0;i<4;i++){
+            for(int j=0;j<6;j++){
+                System.out.print("0");
+            }
+            System.out.println();
+        }
+    }
+}
