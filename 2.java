7355dba9e66921a4126adcf739b2f35a66a086cc
shashank chaudhary <97469450+Shashankch14@users.noreply.github.com>
2024-05-28 00:07:43 +0530

    Create 2.java
    create the given pattern 
    0
    00
    000
    0000

---
+++ 2.java
@@ -0,0 +1,18 @@
+/*create the given pattern 
+0
+00
+000
+0000
+
+*/
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        for(int i =0;i<4;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("0");
+            }
+            System.out.println();
+        }
+    }
+}
